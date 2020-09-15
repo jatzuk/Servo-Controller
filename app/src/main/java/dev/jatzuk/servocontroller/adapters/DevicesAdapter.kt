f7eb221466f671661f445120e79b93e75537af7f
jatzuk <jatzuk.22@gmail.com>
2020-09-16 00:26:24 +0300

    Implemented onClickListener for DevicesAdapter.kt with currently/previously selected item

--- app/src/main/java/dev/jatzuk/servocontroller/adapters/DevicesAdapter.kt
+++ app/src/main/java/dev/jatzuk/servocontroller/adapters/DevicesAdapter.kt
@@ -1,21 +1,47 @@
 package dev.jatzuk.servocontroller.adapters
 
 import android.bluetooth.BluetoothDevice
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import dev.jatzuk.servocontroller.R
 import dev.jatzuk.servocontroller.databinding.ItemDeviceBinding
 
 class DevicesAdapter :
     ListAdapter<BluetoothDevice, DevicesAdapter.ViewHolder>(DevicesDiffCallback()) {
 
+    private var previouslySelectedPosition = -1
+    private var selectedItem = 0
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder.from(parent)
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(getItem(position))
+        val item = getItem(position)
+
+        holder.apply {
+            bind(item)
+            itemView.setOnClickListener {
+                val prevItem = (itemView.parent as RecyclerView).findViewHolderForAdapterPosition(
+                    previouslySelectedPosition
+                )
+                prevItem?.let {
+                    if (previouslySelectedPosition != position && previouslySelectedPosition > -1) {
+                        (it as ViewHolder).reset()
+                    }
+                }
+
+                selectedItem = position
+                holder.onClicked(item)
+                previouslySelectedPosition = position
+            }
+        }
     }
+
+    fun getSelectedItem() = currentList[selectedItem]
 
     class ViewHolder private constructor(
         private val binding: ItemDeviceBinding
@@ -31,6 +57,18 @@ class DevicesAdapter :
 //                ivDeviceIcon =
                 executePendingBindings()
             }
+        }
+
+        fun onClicked(device: BluetoothDevice) {
+            binding.apply {
+                this.device = device
+                ivStatus.setColorFilter(Color.GREEN)
+            }
+        }
+
+        fun reset() {
+            val color = ContextCompat.getColor(itemView.context, R.color.colorPrimary)
+            binding.ivStatus.setColorFilter(color)
         }
 
         companion object {
