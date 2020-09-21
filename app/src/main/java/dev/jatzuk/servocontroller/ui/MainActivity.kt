7b4db396d52634c5f0f327b0c0e17715d185f28f
jatzuk <jatzuk.22@gmail.com>
2020-09-22 01:23:13 +0300

    Drawer navigation layout round corners.

--- app/src/main/java/dev/jatzuk/servocontroller/ui/MainActivity.kt
+++ app/src/main/java/dev/jatzuk/servocontroller/ui/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.shape.CornerFamily
+import com.google.android.material.shape.MaterialShapeDrawable
 import dagger.hilt.android.AndroidEntryPoint
 import dev.jatzuk.servocontroller.R
 import dev.jatzuk.servocontroller.databinding.ActivityMainBinding
@@ -23,6 +25,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val radius = resources.getDimension(R.dimen.side_nav_bar_corner_radius)
+        val navViewBackground = (binding.navView.background as MaterialShapeDrawable)
+        navViewBackground.shapeAppearanceModel = navViewBackground.shapeAppearanceModel.toBuilder()
+            .setTopRightCorner(CornerFamily.ROUNDED, radius)
+            .setBottomRightCorner(CornerFamily.ROUNDED, radius)
+            .build()
+
         setSupportActionBar(binding.drawerContent.toolbar)
 
         navController = findNavController(R.id.nav_host_fragment)
