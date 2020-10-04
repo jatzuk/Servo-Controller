54b300d870ee023e5fc9d7fd1f6aa2252b060a29
jatzuk <jatzuk.22@gmail.com>
2020-10-04 11:23:11 +0300

    Added TestAppModule.kt for Hilt

---
+++ app/src/androidTest/java/dev/jatzuk/servocontroller/di/TestAppModule.kt
@@ -0,0 +1,23 @@
+package dev.jatzuk.servocontroller.di
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dev.jatzuk.servocontroller.db.ServoDatabase
+import javax.inject.Named
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object TestAppModule {
+
+    @Provides
+    @Named("test_db")
+    fun provideInMemoryDb(@ApplicationContext context: Context) =
+        Room.inMemoryDatabaseBuilder(context, ServoDatabase::class.java)
+            .allowMainThreadQueries()
+            .build()
+}
